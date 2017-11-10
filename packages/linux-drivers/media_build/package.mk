dca9a6bfdef9a7df885a73845110725430967ce8
vel2000 <you@example.com>
2017-11-10 16:08:26 +0700

    include aml hdmimonitor

--- packages/linux-drivers/media_build/package.mk
+++ packages/linux-drivers/media_build/package.mk
@@ -49,8 +49,8 @@ make_target() {
     if [ -f $PKG_DIR/config/generic.config ]; then
       cp $PKG_DIR/config/generic.config v4l/.config
     fi
-  elif [ "$PROJECT" != "S805" ] && [ "$PROJECT" != "S905" ] && [ "$PROJECT" != "S912" ]; then
-    if [ -f $PKG_DIR/config/usb.config ]; then
+        elif [ "$PROJECT" != "S805" ] && [ "$PROJECT" != "S905" ] && [ "$PROJECT" != "S912" ]; then
+        if [ -f $PKG_DIR/config/usb.config ]; then
       cp $PKG_DIR/config/usb.config v4l/.config
     fi
   fi
